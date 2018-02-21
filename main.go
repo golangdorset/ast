e08315a357d5ca7b03866e26eead7a35977a693b
codingconcepts <bassrob@gmail.com>
2018-02-21 07:49:11 +0000

    Added example which includes aliases

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	f "fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"log"
+)
 
+func main() {
+	node, err := parser.ParseFile(token.NewFileSet(), "main.go", nil, parser.ImportsOnly)
+	if err != nil {
+		log.Fatalf("error parsing file: %v", err)
+	}
+
+	ast.Inspect(node, func(n ast.Node) bool {
+		// Prints out import paths and grabs the alias if available.
+		if imp, ok := n.(*ast.ImportSpec); ok {
+			if imp.Name != nil {
+				f.Println(imp.Path.Value, imp.Name.Name)
+			} else {
+				f.Println(imp.Path.Value)
+			}
+		}
+		return true
+	})
 }
